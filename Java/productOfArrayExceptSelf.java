17b8baa451024868249beb7a7a0338de6d469565
Sean Nguyen <seannguyen2919@gmail.com>
2022-06-18 15:29:32 -0400

    53, find maximum subarray, using both divide and conquer and simple O(n) traversal

--- Java/productOfArrayExceptSelf.java
+++ Java/productOfArrayExceptSelf.java
@@ -1,20 +1,60 @@
+  //naive, brute force is for each element, consider that and loop through the rest of the indexes to find the element that give the maximum sub array, and then compare it to the overall maximum.
+  //divide and conquer O(nlogn), divide the array into left and right half, find the maxSubArray of left half, right half, and subarray that crosses mid point. then choose the maximum of the three
+  //O(n) solution, traverse through the array once, storing maximum sub-array as we go. If the
+  //sum of current subarray + nums[index] < nums[index] then the parts upto this point is obviously not in the
+  //final solution so change the start of the subarray to this num,
 class Solution {
-    //brute force is for each index, go through the entire array to find the product without that index. obviously o(n^2)
-  //solution, find the left product for each of the element as part 1 of the solution. Then find the right product. Then multiply left and right to get the final solution
-  public int[] productExceptSelf(int[] nums) {
-      int[] result = new int[nums.length];
-      result[0] = 1;
-      //find the left product for each element
-      for (int i = 1; i < nums.length; i ++){
-        result[i] = result[i - 1] * nums[i - 1];
-      }
-      int rightProduct = 1;
-      //find the right product for each element
-      // and multiply with left product for final result
-      for (int i = nums.length - 1; i >= 0; i--){
-         result[i] = result[i] * rightProduct;
-         rightProduct = rightProduct * nums[i];
-      }
-      return result;
-  }
+    // A utility function to find maximum of two integers
+    public int max(int a, int b) { return (a > b) ? a : b; }
+
+    // A utility function to find maximum of three integers
+    public int max(int a, int b, int c) { return max(max(a, b), c); }
+    
+    public int findCrossSum(int[] nums, int start, int mid, int end){
+        //start at mid, find max leftsum
+        int maxLeftSum = -999999;
+        int currentLeftSum = 0;
+        for (int i = mid; i >= start; i --){
+            currentLeftSum =currentLeftSum +  nums[i];
+            maxLeftSum = max(maxLeftSum, currentLeftSum);
+        }
+        int maxRightSum = -999999;
+        int currentRightSum = 0;
+        // start after mid, find max rightsum
+        for (int i = mid + 1; i <= end; i ++){
+            currentRightSum = currentRightSum + nums[i];
+            maxRightSum = max(maxRightSum, currentRightSum);
+        }
+        return(max(maxLeftSum, maxRightSum, (maxLeftSum + maxRightSum)));
+    }
+    
+    public int findMaxSum(int[] nums, int start, int end){
+        if(start==end){ 
+            return nums[start];
+        }
+        int mid = (end+start)/2;
+        return(max(findMaxSum(nums,start, mid), findMaxSum(nums,mid+1, end), findCrossSum(nums,start, mid ,end)));
+    }
+    
+    public int maxSubArray(int[] nums) {
+        return findMaxSum(nums, 0, nums.length - 1);
+    }
+}
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = currentSum;
+        for (int i = 1; i < nums.length; i ++){
+            if (nums[i] > (currentSum + nums[i])){
+                currentSum = nums[i];
+            } else{
+                currentSum += nums[i];
+            }
+            if (currentSum > maxSum){
+                maxSum = currentSum;
+            }
+        }
+        return maxSum;
+    }
 }
